e52e7121507f257332785ca339fc83b33683878c
coolasice1999 <52601736+coolasice1999@users.noreply.github.com>
2019-08-09 20:13:19 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-if [ ! -f /mnt/cron.conf ]; then
-#schedulr run video clean and update daily
-    echo "0 1 * * * /usr/bin/kodi-send -a "UpdateLibrary(video)" > /mnt/cron.conf
-    echo "30 1 * * * /usr/bin/kodi-send -a "CleanLibrary(video)" >> /mnt/cron.conf
-fi
+cp /config/cron.conf /mnt/cron.conf
 chmod 777 /config/*
 chmod 600 /mnt/cron.conf
 crontab /mnt/cron.conf
